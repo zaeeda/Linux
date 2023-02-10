0ae4c0731e04829d082ff932c80a064b52f2be12
zaeeda <oazeedah@gmail.com>
2023-02-09 21:53:06 -0600

    added files to root

---
+++ variable-test.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Author: Zaeedah Akinyemi
+#Date: 02/09/2023
+#Description: This script will run basic variable commands
+#Date Modified:02/09/2023
+
+#Define variables
+p=pwd
+l=ls
+wi=whoami
+d=date
+c='cal 2018'
+
+echo Run variables tasks
+echo
+$p
+$l
+$wi
+$d
+$c
